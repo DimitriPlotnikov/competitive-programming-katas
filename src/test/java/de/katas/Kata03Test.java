f03f52834e8540d9a3852994d22452352bf5f826
dipl <dimitri.plotnikov@gmx.de>
2025-07-23 20:38:01 +0200

    KATA-3: add naive and memoization approach
    Solve the problem with plain recursive algorithm.
    Solve also with more sophisiticated recusive
    alorithim that uses memoization.

---
+++ src/test/java/de/katas/Kata03Test.java
@@ -0,0 +1,36 @@
+package de.katas;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests some expected and corner cases.
+ */
+public class Kata03Test {
+
+  @Test
+  public void input1() {
+    String word1 = "horse", word2 = "ros";
+    
+    Assertions.assertEquals(3, new Kata03().minDistance(word1, word2));
+  }
+  
+  @Test
+  public void input2() {
+    String word1 = "intention", word2 = "execution";
+    
+    Kata03 kata03 = new Kata03();
+    
+    Assertions.assertEquals(5, kata03.minDistance(word1, word2));
+  }
+  
+  @Test
+  public void input3() {
+    String word1 = "dinitrophenylhydrazine", word2 = "acetylphenylhydrazine";
+    
+    Kata03 kata03 = new Kata03();
+    
+    Assertions.assertEquals(6, kata03.minDistance(word1, word2));
+  }
+
+}
