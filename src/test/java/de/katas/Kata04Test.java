e8ec7c0609570c793f0b0d5cb5210988899b30e4
dipl <dimitri.plotnikov@gmx.de>
2025-08-05 22:09:03 +0200

    KATA4: add template for new problem

---
+++ src/test/java/de/katas/Kata04Test.java
@@ -0,0 +1,35 @@
+package de.katas;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Kata04Test {
+
+
+  @Test
+  public void testInput01() {
+    int[] nums = new int[] {1, 1, 2}; // Input array
+    int[] expectedNums = new int[] {1, 2}; // The expected answer with correct length
+
+    int k = new Kata04().removeDuplicates(nums); // Calls your implementation
+
+    Assertions.assertEquals(expectedNums.length, k);
+    for (int i = 0; i < k; i++) {
+      assert nums[i] == expectedNums[i];
+    }
+  }
+
+  @Test
+  public void testInput02() {
+    int[] nums = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4}; // Input array
+    int[] expectedNums = new int[] {0, 1, 2, 3, 4}; // The expected answer with correct length
+
+    int k = new Kata04().removeDuplicates(nums); // Calls your implementation
+
+    Assertions.assertEquals(expectedNums.length, k);
+    for (int i = 0; i < k; i++) {
+      assert nums[i] == expectedNums[i];
+    }
+  }
+
+}
