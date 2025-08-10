1a4937f6375a0bfdce9b2566145c3586c504da1c
dipl <dimitri.plotnikov@gmx.de>
2025-08-10 22:27:46 +0200

    KATA-4: inplace solution
    Maintain 2 indexes for read and write of numbers. Skip all repeated.

--- src/main/java/de/katas/Kata04.java
+++ src/main/java/de/katas/Kata04.java
@@ -1,9 +1,22 @@
 package de.katas;
+
 /**
- * Remove Duplicates from Sorted Array  
+ * Remove Duplicates from Sorted Array
  */
 public class Kata04 {
+  /**
+   * @param nums Constraints: 1 <= nums.length <= 3 * 104 -100 <= nums[i] <= 100
+   * 
+   * @return nums is sorted in non-decreasing order.
+   */
   public int removeDuplicates(int[] nums) {
-    return 0;
+    int write = 1;
+
+    for (int read = 1; read < nums.length; ++read) {
+      if (nums[read - 1] != nums[read]) {
+        nums[write++] = nums[read];
+      }
+    }
+    return write;
   }
 }
