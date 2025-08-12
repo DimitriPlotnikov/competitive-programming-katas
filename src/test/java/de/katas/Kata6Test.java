d8ad00e1095e02afb5aeb8d6b243d93b149b59a4
dipl <dimitri.plotnikov@gmx.de>
2025-08-12 22:21:41 +0200

    KATA6: solve removal through bitmap tabale

---
+++ src/test/java/de/katas/Kata6Test.java
@@ -0,0 +1,28 @@
+package de.katas;
+
+import java.util.Collections;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Kata6Test {
+
+  @Test
+  public void testInput1() {
+    int[] nums = new int[] {3, 2, 2, 3}; // Input array
+    int val = 3; // Value to remove
+    int[] expectedNums = new int[] {2, 2}; // The expected answer with correct length.
+    // It is sorted with no values equaling val.
+
+    int k = new Kata6().removeElement(nums, val); // Calls your implementation
+
+    assertSolution(nums, expectedNums, k);
+  }
+
+  private void assertSolution(int[] nums, int[] expectedNums, int k) {
+    Assertions.assertEquals(expectedNums.length, k);
+    
+    for (int i = 0; i < expectedNums.length; i++) {
+      Assertions.assertEquals(expectedNums[i], nums[i]);
+    }
+  }
+}
